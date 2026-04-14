07a36891225320ee32bad99df48bb7d3fe62100f
Bobkata14 <151872672+Bobkata14@users.noreply.github.com>
2026-04-14 14:36:49 +0300

    Add files via upload
    Exercises with print statement, and mini Python project.

---
+++ hello.py
@@ -0,0 +1,38 @@
+#ЗАДАЧА 1
+print ("Hello World")
+
+#ЗАДАЧА 2
+x = 5
+y = 10
+
+print(x+y)
+
+#ЗАДАЧА 3
+if 5 > 2:
+  print("Five is greater than two")
+
+
+#ЗАДАЧА 4
+print("Hello")
+
+#ЗАДАЧА 5
+#Този код събира x и y
+x = 10
+y = 20
+print(x + y)
+
+#ЗАДАЧА 6
+name = "Boris"
+
+print("Hello", name)
+
+#ЗАДАЧА 7
+#FALSE
+
+#ЗАДАЧА 8
+if True:
+    print("Yes")
+else:
+    print("No")
+
+#OVERALL - 5.50-6.00
---
+++ miniproject.py
@@ -0,0 +1,15 @@
+#USER'S BUSINESS CARD
+
+print("--- User profile ---\n")
+
+name = "Boris"
+age = 23
+city = "Sofia"
+favorite_number = 14
+
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"City: {city}")
+print(f"Favorite_number: {favorite_number}")
+
+#OVERALL - 6.00
